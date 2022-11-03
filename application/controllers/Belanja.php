02915a50b11a9624c00060aba85ab475795c4393
mramadhanurh <mramadhanur@gmail.com>
2022-11-03 16:38:40 +0700

    shopping cart | update dan delete keranjang (front end)

--- application/controllers/Belanja.php
+++ application/controllers/Belanja.php
@@ -5,6 +5,9 @@ class Belanja extends CI_Controller {
 
 	public function index()
 	{
+        if (empty($this->cart->contents())) {
+            redirect('home');
+        }
 		$data = array(
             'title' => 'Keranjang Belanja',
             'isi' => 'v_belanja',
@@ -23,6 +26,32 @@ class Belanja extends CI_Controller {
         );        
         $this->cart->insert($data);
         redirect($redirect_page, 'refresh');
+    }
+
+    public function update()
+    {
+        $i = 1;
+        foreach ($this->cart->contents() as $items) {
+            $data = array(
+                'rowid' => $items['rowid'],
+                'qty'   => $this->input->post($i . '[qty]'),
+            );
+            $this->cart->update($data);
+            $i++;
+        }
+        redirect('belanja');
+    }
+
+    public function delete($rowid)
+    {
+        $this->cart->remove($rowid);
+        redirect('belanja');
+    }
+
+    public function clear()
+    {
+        $this->cart->destroy();
+        redirect('belanja');
     }
 
 }
--- application/views/v_belanja.php
+++ application/views/v_belanja.php
@@ -24,16 +24,17 @@
                                     'name' => $i.'[qty]', 
                                     'value' => $items['qty'], 
                                     'maxlength' => '3', 
+                                    'min' => '0',
                                     'size' => '5', 
                                     'type' => 'number',
                                     'class' => 'form-control'
                                 )); ?>
                             </td>
                             <td><?php echo $items['name']; ?></td>
-                            <td style="text-align:right">Rp.<?php echo $this->cart->format_number($items['price']); ?></td>
-                            <td style="text-align:right">Rp.<?php echo $this->cart->format_number($items['subtotal']); ?></td>
+                            <td style="text-align:right">Rp. <?php echo number_format($items['price'], 0); ?></td>
+                            <td style="text-align:right">Rp. <?php echo number_format($items['subtotal'], 0); ?></td>
                             <td class="text-center">
-                                <a href="" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+                                <a href="<?= base_url('belanja/delete/' . $items['rowid']) ?>" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
                             </td>
                     </tr>
 
@@ -44,12 +45,13 @@
             <tr>
                     <td colspan="2"> </td>
                     <td class="right"><h3>Total :</h3></td>
-                    <td class="right"><h3>Rp.<?php echo $this->cart->format_number($this->cart->total()); ?></h3></td>
+                    <td class="right"><h3>Rp. <?php echo number_format($this->cart->total(), 0); ?></h3></td>
             </tr>
 
             </table>
 
-            <button type="submit" class="btn btn-primary btn-flat"><i class="fa fa-save"></i> Update Chart</button>
+            <button type="submit" class="btn btn-primary btn-flat"><i class="fa fa-save"></i> Update Cart</button>
+            <a href="<?= base_url('belanja/clear') ?>" class="btn btn-danger btn-flat"><i class="fa fa-recycle"></i> Clear Cart</a>
             <a href="#" class="btn btn-success btn-flat"><i class="fa fa-check-square"></i> Check Out</a>
             <?php echo form_close(); ?>
             <br>
